73fa76154ff72dedbdcbdd1517c607f0176d9035
新元noname <13257656+xinyuanwm@user.noreply.gitee.com>
2026-02-19 11:53:55 +0800

    feat(database): 添加数据库升级脚本和相关工具函数
    添加了数据库升级功能，包括数据迁移和表结构更新。
    新增 upgrade 脚本用于处理数据库版本升级，
    添加多个数据库操作工具函数用于备份、迁移和管理表结构。

--- src/database/execSql.js
+++ src/database/execSql.js
@@ -27,10 +27,72 @@ function execSqlFiles(db, ...paths) {
         }
     }
 }
+/**
+ * 
+ * @param {import("better-sqlite3").Database} db 
+ */
 function execOpenForeignKeys(db) {
     db.exec('PRAGMA foreign_keys = ON;');
 }
+/**
+ * 
+ * @param {import("better-sqlite3").Database} db 
+ */
+function execGetAllTableNames(db) {
+    return db.prepare(`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'`).all().map(o => o.name);
+}
+/**
+ * 
+ * @param {import("better-sqlite3").Database} db 
+ * @param {string[]} tables 
+ */
+function execGenTableBackup(db, tables) {
+    for (const tableName of tables) {
+        const backupName = `${tableName}_backup`;
+        db.exec(`DROP TABLE IF EXISTS ${backupName}`);
+        db.exec(`CREATE TABLE ${backupName} AS SELECT * FROM ${tableName}`);
+    }
+}
+/**
+ * 
+ * @param {import("better-sqlite3").Database} db 
+ * @param {string[]} tables 
+ * @param {(x:string)=>string} alias
+ */
+function execDropTables(db, tables, alias = x => x) {
+    for (const tableName of tables) {
+        const aliasName = alias(tableName);
+        db.exec(`DROP TABLE IF EXISTS ${aliasName}`);
+    }
+}
+/**
+ * 
+ * @param {import("better-sqlite3").Database} db 
+ * 
+ *
+ */
+function execGetCommonCols(db, newTableName, oldTableName) {
+    const oldCols = db.prepare(`PRAGMA table_info(${oldTableName})`).all();
+    const newCols = db.prepare(`PRAGMA table_info(${newTableName})`).all();
+    const commonCols = oldCols
+        .filter(old => newCols.find(newCol => newCol.name === old.name))
+        .map(c => c.name);
+    console.log(oldCols, newCols)
+    return commonCols;
+}
+function execMigrateCommonCols(db, newTableName,  oldTableName,commonCols) {
+    const joinedCols = commonCols.join(",")
+    db.exec(`
+      INSERT INTO ${newTableName} (${joinedCols}) 
+      SELECT ${joinedCols} FROM ${oldTableName}
+    `);
+}
 module.exports = {
     execSqlFiles,
-    execOpenForeignKeys
+    execOpenForeignKeys,
+    execGetAllTableNames,
+    execGenTableBackup,
+    execDropTables,
+    execGetCommonCols,
+    execMigrateCommonCols
 };
